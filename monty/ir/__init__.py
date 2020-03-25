b0fd3e7b80284d5aa839b0f4c114ba590b04fdab
mental <m3nta1@yahoo.com>
2020-03-25 18:32:49 +0000

    Change SSAValue to be a Tuple[int, int] instead of int

--- monty/ir/__init__.py
+++ monty/ir/__init__.py
@@ -1,10 +1,6 @@
 from typing import NewType, TypeVar, Dict
 
-SSAValue = NewType("SSAValue", int)
-
-V = TypeVar("V")
-def _next_ssa_value(mapping: Dict[SSAValue, V]) -> SSAValue:
-    return SSAValue(max(mapping) + 1 if mapping else 0)
+SSAValue = NewType("SSAValue", lambda block, discrim: (block, discrim))
 
 from .raw_type import *
 from .block import *
