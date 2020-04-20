35691b10d82f1857ca6482f69052efa57b5f1ae1
mental <m3nta1@yahoo.com>
2020-04-20 20:33:52 +0100

    Add monty.mir.scope.Import

--- monty/mir/scope.py
+++ monty/mir/scope.py
@@ -1,14 +1,25 @@
 from dataclasses import dataclass, field
 from typing import List, Dict, Set, Union
 
-__all__ = ("Scope",)
+__all__ = ("Scope", "Import")
+
+
+@dataclass
+class Import:
+    """An import."""
+    name: str
+    alias: str
+    level: int
+
+    def __hash__(self) -> int:
+        return hash((self.name, self.alias, self.level))
 
 
 @dataclass
 class Scope:
     """A semantic scope."""
 
-    imports: Set[str] = field(default_factory=set)
+    imports: Set[Import] = field(default_factory=set)
     names: Dict[str, "RawType"] = field(default_factory=dict)
 
     def __setitem__(self, key: str, value: Union["RawType", "Argument"]):
