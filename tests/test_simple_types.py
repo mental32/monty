af27aff726792081ae448f041c7f77cdd99e3582
mental <m3nta1@yahoo.com>
2020-09-22 21:35:45 +0100

    tests: add simple type reconstruction test.

---
+++ tests/test_simple_types.py
@@ -0,0 +1,18 @@
+import monty
+
+s = """
+def oof(y: int, x: str) -> bool:
+    return True
+
+def foo(x: str, y: int) -> bool:
+    return False
+"""
+
+def test_simple_function_types():
+    code = monty.compile(s)
+
+    oof = code.functions["oof"]
+    assert code.tcx.reconstruct(oof.type_id) == "Callable[Tuple[int, str], bool]"
+
+    foo = code.functions["foo"]
+    assert code.tcx.reconstruct(foo.type_id) == "Callable[Tuple[str, int], bool]"
