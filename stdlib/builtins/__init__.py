73de33bcfb35440975c2682a7a835a2cd7f2075f
mental <m3nta1@yahoo.com>
2020-09-27 14:22:50 +0300

    builtins: Update `print` impl

--- stdlib/builtins/__init__.py
+++ stdlib/builtins/__init__.py
@@ -1,1 +1,12 @@
-bar: str = "foo"
+import __monty
+
+assert __monty.arch.os == "linux-gnu"
+
+
+def print(st: str):
+    stdout: int = __monty.libc.fopen(1, "w")
+    __monty.libc.fwrite(st, 1, len(st), stdout)
+    __monty.libc.fflush(stdout)
+
+def len(st: str) -> int:
+    return 20
