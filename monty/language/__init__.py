6ccf4e71008c6a08522a51f042bf92b878f83d34
mental <m3nta1@yahoo.com>
2020-10-08 00:00:20 +0300

    Start again!

--- monty/language/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .klass import *
-from .import_decl import *
-from .module import *
-from .scope import *
-from .func import *
-from .item import *
--- monty/typing/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = ("TypeId", "TypeInfo", "Primitive", "List", "Callable", "Ref")
-
-TypeId = int
-
-from .type_info import *
-from .primitives import *
-from .compound import *
-from .type_context import *
