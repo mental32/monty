50cbe5e74798af19cafc041dee3588e1a69439cd
mental <m3nta1@yahoo.com>
2021-05-11 05:32:24 +0300

    fix(typecheck): forgot to typecheck the test of an `If`

--- montyc/src/ast/ifelif.rs
+++ montyc/src/ast/ifelif.rs
@@ -56,6 +56,8 @@ impl TypedObject for If {
         let mut ctx = ctx.clone();
         ctx.this = Some(self.test.clone());
 
+        ctx.with(Rc::clone(&self.test), |ctx, test| test.typecheck(&ctx))?;
+
         let test_t = ctx.with(Rc::clone(&self.test), |ctx, test| test.infer_type(&ctx))?;
 
         if !ctx.global_context.type_map.type_eq(test_t, TypeMap::BOOL) {
